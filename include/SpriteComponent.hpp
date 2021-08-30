444ebbcc1ba5058b39caf5b8eee1cc08095c2482
alanrostin <alan.rostin@gmail.com>
2021-08-30 18:23:48 +0430

    Modify SpriteComponent class

--- include/SpriteComponent.hpp
+++ include/SpriteComponent.hpp
@@ -3,19 +3,23 @@
 
 #include "Component.hpp"
 #include "TransformComponent.hpp"
+#include "ResourceManager.hpp"
 
 class SpriteComponent : public Component
 {
     public:
         SpriteComponent(Object* object);
 
+        void setTextureManager(ResourceManager<sf::Texture>* resourceManager);
+        
+        void load(int textureId);
         void load(const std::string& filePath);
 
         void lateUpdate(float deltaTime) override;
         void render(Window& window) override;
     
     private:
-        sf::Texture texture;
+        ResourceManager<sf::Texture>* resourceManager;
         sf::Sprite sprite;
 };
 
--- src/SpriteComponent.cpp
+++ src/SpriteComponent.cpp
@@ -6,10 +6,32 @@ SpriteComponent::SpriteComponent(Object* object) : Component(object)
 
 }
 
+void SpriteComponent::setTextureManager(ResourceManager<sf::Texture>* resourceManager)
+{
+    this -> resourceManager = resourceManager;
+}
+
+void SpriteComponent::load(int textureId)
+{
+    if (textureId >= 0)
+    {
+        std::shared_ptr<sf::Texture> texture = resourceManager -> getResource(textureId);
+        sprite.setTexture(*texture);
+    }
+}
+
 void SpriteComponent::load(const std::string& filePath)
 {
-    texture.loadFromFile(filePath);
-    sprite.setTexture(texture);
+    if (resourceManager)
+    {
+        int textureId = resourceManager -> addResource(filePath);
+
+        if (textureId >= 0)
+        {
+            std::shared_ptr<sf::Texture> texture = resourceManager -> getResource(textureId);
+            sprite.setTexture(*texture);
+        }
+    }
 }
 
 void SpriteComponent::lateUpdate(float deltaTime)
